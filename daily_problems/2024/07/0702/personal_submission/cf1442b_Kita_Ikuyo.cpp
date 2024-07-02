cb3df2043c00a0c63a804d8ac1208e3470b95488
alex-2354 <128209124+alex-2354@users.noreply.github.com>
2024-07-02 23:50:13 +0800

    Create cf1442b_Kita_Ikuyo.cpp

---
+++ daily_problems/2024/07/0702/personal_submission/cf1442b_Kita_Ikuyo.cpp
@@ -0,0 +1,254 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define IOS ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define int long long
+
+#ifdef LOCAL
+#include "C:\Program Files\DEBUG\debug.h"
+#else
+#define debug(...) 114514
+#define ps 114514
+#endif
+
+using i64 = long long;
+
+template <class T>
+constexpr T power(T a, i64 b) {
+    T res = 1;
+    for (; b; b /= 2, a *= a) {
+        if (b % 2) {
+            res *= a;
+        }
+    }
+    return res;
+}
+
+constexpr i64 mul(i64 a, i64 b, i64 p) {
+    i64 res = a * b - i64(1.L * a * b / p) * p;
+    res %= p;
+    if (res < 0) {
+        res += p;
+    }
+    return res;
+}
+template <i64 P>
+struct MLong {
+    i64 x;
+    constexpr MLong() : x{} {}
+    constexpr MLong(i64 x) : x{norm(x % getMod())} {}
+
+    static i64 Mod;
+    constexpr static i64 getMod() {
+        if (P > 0) {
+            return P;
+        } else {
+            return Mod;
+        }
+    }
+    constexpr static void setMod(i64 Mod_) { Mod = Mod_; }
+    constexpr i64 norm(i64 x) const {
+        if (x < 0) {
+            x += getMod();
+        }
+        if (x >= getMod()) {
+            x -= getMod();
+        }
+        return x;
+    }
+    constexpr i64 val() const { return x; }
+    explicit constexpr operator i64() const { return x; }
+    constexpr MLong operator-() const {
+        MLong res;
+        res.x = norm(getMod() - x);
+        return res;
+    }
+    constexpr MLong inv() const {
+        assert(x != 0);
+        return power(*this, getMod() - 2);
+    }
+    constexpr MLong& operator*=(MLong rhs) & {
+        x = mul(x, rhs.x, getMod());
+        return *this;
+    }
+    constexpr MLong& operator+=(MLong rhs) & {
+        x = norm(x + rhs.x);
+        return *this;
+    }
+    constexpr MLong& operator-=(MLong rhs) & {
+        x = norm(x - rhs.x);
+        return *this;
+    }
+    constexpr MLong& operator/=(MLong rhs) & { return *this *= rhs.inv(); }
+    friend constexpr MLong operator*(MLong lhs, MLong rhs) {
+        MLong res = lhs;
+        res *= rhs;
+        return res;
+    }
+    friend constexpr MLong operator+(MLong lhs, MLong rhs) {
+        MLong res = lhs;
+        res += rhs;
+        return res;
+    }
+    friend constexpr MLong operator-(MLong lhs, MLong rhs) {
+        MLong res = lhs;
+        res -= rhs;
+        return res;
+    }
+    friend constexpr MLong operator/(MLong lhs, MLong rhs) {
+        MLong res = lhs;
+        res /= rhs;
+        return res;
+    }
+    friend constexpr std::istream& operator>>(std::istream& is, MLong& a) {
+        i64 v;
+        is >> v;
+        a = MLong(v);
+        return is;
+    }
+    friend constexpr std::ostream& operator<<(std::ostream& os,
+                                              const MLong& a) {
+        return os << a.val();
+    }
+    friend constexpr bool operator==(MLong lhs, MLong rhs) {
+        return lhs.val() == rhs.val();
+    }
+    friend constexpr bool operator!=(MLong lhs, MLong rhs) {
+        return lhs.val() != rhs.val();
+    }
+};
+
+template <>
+i64 MLong<0LL>::Mod = i64(1E18) + 9;
+
+template <int P>
+struct MInt {
+    int x;
+    constexpr MInt() : x{} {}
+    constexpr MInt(i64 x) : x{norm(x % getMod())} {}
+
+    static int Mod;
+    constexpr static int getMod() {
+        if (P > 0) {
+            return P;
+        } else {
+            return Mod;
+        }
+    }
+    constexpr static void setMod(int Mod_) { Mod = Mod_; }
+    constexpr int norm(int x) const {
+        if (x < 0) {
+            x += getMod();
+        }
+        if (x >= getMod()) {
+            x -= getMod();
+        }
+        return x;
+    }
+    constexpr int val() const { return x; }
+    explicit constexpr operator int() const { return x; }
+    constexpr MInt operator-() const {
+        MInt res;
+        res.x = norm(getMod() - x);
+        return res;
+    }
+    constexpr MInt inv() const {
+        assert(x != 0);
+        return power(*this, getMod() - 2);
+    }
+    constexpr MInt& operator*=(MInt rhs) & {
+        x = 1LL * x * rhs.x % getMod();
+        return *this;
+    }
+    constexpr MInt& operator+=(MInt rhs) & {
+        x = norm(x + rhs.x);
+        return *this;
+    }
+    constexpr MInt& operator-=(MInt rhs) & {
+        x = norm(x - rhs.x);
+        return *this;
+    }
+    constexpr MInt& operator/=(MInt rhs) & { return *this *= rhs.inv(); }
+    friend constexpr MInt operator*(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res *= rhs;
+        return res;
+    }
+    friend constexpr MInt operator+(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res += rhs;
+        return res;
+    }
+    friend constexpr MInt operator-(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res -= rhs;
+        return res;
+    }
+    friend constexpr MInt operator/(MInt lhs, MInt rhs) {
+        MInt res = lhs;
+        res /= rhs;
+        return res;
+    }
+    friend constexpr std::istream& operator>>(std::istream& is, MInt& a) {
+        i64 v;
+        is >> v;
+        a = MInt(v);
+        return is;
+    }
+    friend constexpr std::ostream& operator<<(std::ostream& os, const MInt& a) {
+        return os << a.val();
+    }
+    friend constexpr bool operator==(MInt lhs, MInt rhs) {
+        return lhs.val() == rhs.val();
+    }
+    friend constexpr bool operator!=(MInt lhs, MInt rhs) {
+        return lhs.val() != rhs.val();
+    }
+};
+
+template <>
+int MInt<0>::Mod = 998244353;
+
+template <int V, int P>
+constexpr MInt<P> CInv = MInt<P>(V).inv();
+
+constexpr int P = 998244353;
+using Z = MInt<P>;
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(n + 1), b(m + 1), vis(n + 1);
+    vector<int> pre(n + 1), nxt(n + 2);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 1; i <= m; i++) {
+        cin >> b[i];
+        vis[b[i]] = 1;
+    }
+    for (int i = 1; i <= n; i++) {
+        pre[a[i]] = (i == 1 ? 0 : a[i - 1]);
+        nxt[a[i]] = (i == n ? 0 : a[i + 1]);
+    }
+
+    Z ans = 1;
+    for (int i = 1; i <= m; i++) {
+        bool ok1 = (pre[b[i]] != 0 && vis[pre[b[i]]] == 0);
+        bool ok2 = (nxt[b[i]] != 0 && vis[nxt[b[i]]] == 0);
+        if (ok1 && ok2) {
+            ans *= 2;
+        } else if (!ok1 && !ok2) {
+            ans = 0;
+        }
+        vis[b[i]] = 0;
+    }
+    cout << ans << endl;
+}
+
+signed main() {
+    IOS int t = 1;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
